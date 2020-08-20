c9121931eeb5ae5916ea67dba8351cec7fa7094e
collinskoech11 <collinskoechck34@gmail.com>
2020-08-20 15:49:16 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+
+
+
+print('Enter your name : ')
+x = input()
+print('hello '+ x)
+print('Your Interest calculator')
+
+print('Principal amount :')
+p = int(input())
+print('Time :')
+t = int(input())
+print('Interest rate :')
+r = int(input())
+
+si=str((p*r*t)/100)
+
+print('Your Interest is : '+ si)

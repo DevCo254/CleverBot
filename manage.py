8dcb467d17cad9b97f1aeb3181d968ebca68039b
collinskoech11 <collinskoechck34@gmail.com>
2020-08-20 19:41:16 +0300

    compound interest calculator

--- manage.py
+++ manage.py
@@ -3,12 +3,21 @@ x = input()
 print('hello '+ x)
 print('Your Interest calculator')
 
+print('Enter 1 for compound interest, or 2 for simple interest')
+y = input()
+if y == 1:
+    P = int(input("Enter starting principle please. "))
+    n = int(input("Enter Compound intrest rate.(daily, monthly, quarterly, half-year, yearly) "))
+    r = float(input("Enter annual interest amount. (decimal) "))
+    t = int(input("Enter the amount of years. "))
+    final = (P * (((1 + (r/n)) ** (n*t))))+P
+else :
+    print('Principal amount :')
+    P = int(input())
+    print('Time :')
+    t = int(input())
+    print('Interest rate :')
+    r = float(input())
 
-P = int(input("Enter starting principle please. "))
-n = int(input("Enter Compound intrest rate.(daily, monthly, quarterly, half-year, yearly) "))
-r = float(input("Enter annual interest amount. (decimal) "))
-t = int(input("Enter the amount of years. "))
-
-final = P * (((1 + (r/n)) ** (n*t)))
-
+    final = ((P*r*t)/100)+P
 print ("The final amount after", t, "years is", final)
